d441ec7d460231aeecd259a145289e07b2f17a10
Dapy <65035781+Dapy0@users.noreply.github.com>
2025-11-15 15:42:44 +0100

    Finished MainLayout

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,6 +1,7 @@
-import { Outlet } from "@tanstack/react-router";
+import { Link, Outlet } from "@tanstack/react-router";
 import PieChartIcon from "./Logo";
 import { SideBarBtn } from "./SideBarBtn";
+import { IconChartAreaLine, IconWallet } from "@tabler/icons-react";
 
 export const MainLayout = () => {
   const DEVtoggleTheme = () => {
@@ -8,19 +9,30 @@ export const MainLayout = () => {
   };
 
   return (
-    <div className="flex h-screen bg-background dark:bg-background-d">
-      
+    <div className="flex h-screen bg-background  dark:text-white">
+
       <button className="absolute size-10 top-5 right-5" onClick={DEVtoggleTheme}>Swap</button>
 
-      <aside className="p-6 bg-foreground dark:bg-foreground-d border-r border-r-primary-900">
+      <aside className="p-4 min-w-60 bg-foreground dark:bg-foreground-d border-r border-r-gray-300 dark:border-r-gray-700 flex flex-col">
         <div className="flex gap-2 items-center">
           <PieChartIcon className="size-10 text-blue-500" />
-          <h3 className="text-2xl font-bold  dark:text-white">CoinPulse</h3>
+          <h3 className="text-2xl font-bold ">CoinPulse</h3>
         </div>
-        <div className="flex flex-col gap-4">
-          <SideBarBtn to={'/'}>Market</SideBarBtn>
-          <SideBarBtn to={'/portfolio'}>Portfolio</SideBarBtn>
-          <SideBarBtn>News</SideBarBtn>
+        <div className="flex flex-col flex-1 gap-4 mt-8">
+          <SideBarBtn to={'/'}><IconChartAreaLine /> Market</SideBarBtn>
+          <SideBarBtn to={'/portfolio'} ><IconWallet />Portfolio</SideBarBtn>
+          {/* <SideBarBtn to={"/news"}>News</SideBarBtn> */}
+        </div>
+        <div className="flex flex-col gap-3">
+          <hr className="border-gray-300 dark:border-gray-700" />
+
+          <div className="flex gap-3 max-w-10 items-center">
+            <img className="size-fit rounded-full" src="https://lh3.googleusercontent.com/aida-public/AB6AXuC9bfBo_L7BNZePomtQc7t__D1KOK6tEa8wlXClIpMd8cSl0f0PoOW6gRtvrShjqjYd8TH9OKo9qO4Xszt0_xMPa5S-UDERKVfWMHEvcrY4d0EUNB-WapI19oA9IQQAqkD_w8A4ekEYEFuVr7ZmaYnl0yqQGDtMgN3sX21pOtj3i2EGfWrKvyBOM4wGBnyGe_XxdqKqQJ1EeQXcOfj3nItbharMa2vcdthJK7V1iFFNothQV7NOg3d5470bogGng4PDC6c8GQWUIMdf" alt="img" />
+            <div className="">
+              <p className="text-sm font-medium ">Alexey</p>
+              <Link className=" text-xs text-gray-600 dark:text-gray-400 hover:text-primary" to={"/"}>Settings</Link>
+            </div>
+          </div>
         </div>
       </aside>
 
--- src/components/SideBarBtn/types.ts
+++ src/components/SideBarBtn/types.ts
@@ -1,6 +1,7 @@
 import type { LinkProps } from "@tanstack/react-router"
+import type { ReactNode } from "react"
 
 export interface ICustomButtonProps extends LinkProps {
-  // children: ReactNode
-  variant?: 'primary' | 'secondary' 
+  children: ReactNode
+  // variant?: 'primary' | 'secondary'
 }
